ed2ae119e9aa6fd77b303b8b20d9c8ffb2b69e9a
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2021-11-24 08:29:06 +0700

    Update extra.js

--- commands/core/extra.js
+++ commands/core/extra.js
@@ -2,11 +2,18 @@ const fetch = require('node-fetch')
 module.exports = {
 name: ["donasi"],
 type: ["default"],
-description: "no Comment",
-utilisation: userbot.prefix + "extra",
+description: "mendonasikan uang anda ke creator",
+utilisation: userbot.prefix + "donasi",
 
 async execute(m) {
 let { conn } = data
 
-} 
+let capt = `
+HALO KAK ${conn.getName(m.sender)}
+Ingin berdonasi Untuk mendukung Creator/Pemilik Bot?
+Pilih Via Donasi Lewat Tombol dibawah!
+`
+
+conn.send3ButLoc(m.chat, await ( await fetch("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWANV5KqHX-ewk_-hP3tU9Ery4CubWvyYbvQ&usqp=CAU")).buffer(), capt, userbot.packname, 'DANA', 'dana', 'Pulsa', 'pulsa', m)
+}
 }
